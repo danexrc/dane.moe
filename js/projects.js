21ac3b905027ee4cc5bfb1f654eb3b69c41ae082
Dane Buchanan <daneb@broadbandtap.co.uk>
2023-11-29 22:22:58 +0000

    Completed projects js implementation

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ let graphics = [];
 let other = [];
 
 function getJSON() {
-    fetch('https://raw.githubusercontent.com/0xdane/dane.moe/main/assets/json/projects.json?token=GHSAT0AAAAAACJJT3K5SSC6MA4RMFEN3KIEZLHIKYQ').then (data => data.json()).then(data => {
+    fetch('https://raw.githubusercontent.com/0xdane/dane.moe/main/assets/json/projects.json?token=GHSAT0AAAAAACJJT3K5V3M3I6GCQREM7NOQZLHIOXQ').then (data => data.json()).then(data => {
           development.push(data.projects.development);
           graphics.push(data.projects.graphics);
           other.push(data.projects.other);
@@ -36,6 +36,34 @@ function getLanguageColor(language) {
     }
 }
 
+function getGraphicsCategoryColor(category) {
+    switch(category) {
+        case "Logo":
+            return "#ff7366";
+        case "Banner":
+            return "#66b3ff";
+        case "Typography":
+            return "#ffe066";
+        case "Branding":
+            return "#ed66ff";
+        case "3D Model":
+            return "#ff66bd";
+        case "Sketch":
+            return "#66ff7f";
+    }
+}
+
+function getOtherCategoryColor(category) {
+    switch(category) {
+        case "Music":
+            return "#7366ff";
+        case "Video":
+            return "#ffa866";
+        case "Misc":
+            return "#737373";
+    }
+}
+
 function populateDevelopment() {
     if (development[0].length == 0) {
         $("#dev-row").append("<h3 style=\"text-align: center;\">There are currently no development projects!</h3>")
@@ -49,19 +77,18 @@ function populateDevelopment() {
                     "<div class=\"col-sm-6\">" +
                     "<div class=\"card d-flex flex-column\">" +
                     "<div class=\"card-body\">" +
-                    "<img class=\"card-img-top responsive\" src=" + element.coverImage + " alt=\"Project image\">" +
-                    "<br>" +
+                    "<img class=\"card-img-top responsive\" src=" + element.coverImage +" style=\"height: 197px;\" alt=\"Project image\">" +
                     "<a class=\"title-link\" href=" + element.sourceLink + "><h5 class=\"card-title\">" + element.title + "</h5></a>" +
                     "<p class=\"card-text\">" + element.description + "</p>" +
                     "<div class=\"proj-details\">" +
                     "<p class=\"card-language\"><span style=color:" + langColor + "><i class=\"fa-solid fa-circle\"></i></span> " + element.language + "</p>" +
-                    "<a class=\"repo-name\" href=" + element.sourceLink + "><p><i class=\"fa-solid fa-code-branch\"></i> " + element.repoName + "</p>" +
+                    "<p><i class=\"fa-solid fa-code-branch\"></i><a class=\"repo-name\" href=" + element.sourceLink + "> " + element.repoName + "</p>" +
                     "</div>" +
                     "</div>" +
                     "</div>" +
                     "</div>"
                 )
-            } else {
+            }
                 $("#dev-row").append(
                     "<div class=\"col-sm-6\">" +
                     "<div class=\"card d-flex flex-column\">" +
@@ -79,42 +106,88 @@ function populateDevelopment() {
             }
         }
     }
-}
 
 function populateGraphics() {
     if (graphics[0].length == 0) {
         $("#graphics-row").append("<h3 style=\"text-align: center;\">There are currently no graphics projects!</h3>")
     } else {
-        for (let i = 0; i < development[0].length; i++) {
-            $("#graphics-row").append(
-                "<div class=\"col-sm-6\">" +
-                "<div class=\"card d-flex flex-column\">" +
-                "<div class=\"card-body\">" +
-                "<a href=" + graphics[0][i].sourceLink + "><h5 class=\"card-title\">" + graphics[0][i].title + "</h5></a>" +
-                "<p class=\"card-text\">" + graphics[0][i].description + "</p>" +
-                "</div>" +
-                "</div>" +
-                "</div>"
-            )
+        for (const element of graphics[0]) {
+
+            let category = element.category;
+            let categoryColor = getGraphicsCategoryColor(category);
+
+            if (element.coverImage != "") {
+                $("#graphics-row").append(
+                    "<div class=\"col-sm-6\">" +
+                    "<div class=\"card d-flex flex-column\">" +
+                    "<div class=\"card-body\">" +
+                    "<img class=\"card-img-top responsive\" src=" + element.coverImage +" style=\"height: 197px;\" alt=\"Project image\">" +
+                    "<a class=\"title-link\" href=" + element.sourceLink + "><h5 class=\"card-title\">" + element.title + "</h5></a>" +
+                    "<p class=\"card-text\">" + element.description + "</p>" +
+                    "<div class=\"proj-details\">" +
+                    "<p class=\"card-category\"><span style=color:" + categoryColor + "><i class=\"fa-solid fa-circle\"></i></span> " + element.category + "</p>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>"
+                )
+            } else {
+                $("#graphics-row").append(
+                    "<div class=\"col-sm-6\">" +
+                    "<div class=\"card d-flex flex-column\">" +
+                    "<div class=\"card-body\">" +
+                    "<a class=\"title-link\" href=" + element.sourceLink + "><h5 class=\"card-title\">" + element.title + "</h5></a>" +
+                    "<p class=\"card-text\">" + element.description + "</p>" +
+                    "<div class=\"proj-details\">" +
+                    "<p class=\"card-category\"><span style=color:" + categoryColor + "><i class=\"fa-solid fa-circle\"></i></span> " + element.category + "</p>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>"
+                )
+            }
         }
     }
 }
 
 function populateOther() {
     if (other[0].length == 0) {
-        $("#other-row").append("<h3 style=\"text-align: center;\">There are currently no music projects!</h3>")
+        $("#other-row").append("<h3 style=\"text-align: center;\">There are currently no other projects!</h3>")
     } else {
-        for (let i = 0; i < other[0].length; i++) {
-            $("#other-row").append(
-                "<div class=\"col-sm-6\">" +
-                "<div class=\"card d-flex flex-column\">" +
-                "<div class=\"card-body\">" +
-                "<a href=" + music[0][i].sourceLink + "><h5 class=\"card-title\">" + music[0][i].title + "</h5></a>" +
-                "<p class=\"card-text\">" + music[0][i].description + "</p>" +
-                "</div>" +
-                "</div>" +
-                "</div>"
-            )
+        for (let element of other[0]) {
+
+            let category = element.category;
+            let categoryColor = getOtherCategoryColor(category);
+
+            if (element.coverImage != "") {
+                $("#other-row").append(
+                    "<div class=\"col-sm-6\">" +
+                    "<div class=\"card d-flex flex-column\">" +
+                    "<div class=\"card-body\">" +
+                    "<img class=\"card-img-top responsive\" src=" + element.coverImage +" style=\"height: 197px;\" alt=\"Project image\">" +
+                    "<a class=\"title-link\" href=" + element.sourceLink + "><h5 class=\"card-title\">" + element.title + "</h5></a>" +
+                    "<p class=\"card-text\">" + element.description + "</p>" +
+                    "<div class=\"proj-details\">" +
+                    "<p class=\"card-category\"><span style=color:" + categoryColor + "><i class=\"fa-solid fa-circle\"></i></span> " + element.category + "</p>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>"
+                )
+            } else {
+                $("#other-row").append(
+                    "<div class=\"col-sm-6\">" +
+                    "<div class=\"card d-flex flex-column\">" +
+                    "<div class=\"card-body\">" +
+                    "<a class=\"title-link\" href=" + element.sourceLink + "><h5 class=\"card-title\">" + element.title + "</h5></a>" +
+                    "<p class=\"card-text\">" + element.description + "</p>" +
+                    "<div class=\"proj-details\">" +
+                    "<p class=\"card-category\"><span style=color:" + categoryColor + "><i class=\"fa-solid fa-circle\"></i></span> " + element.category + "</p>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>"
+                )
+            }
         }
     }
-}
+} 
