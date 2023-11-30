dfee6e071d91c5b5b1b751eea88ae843a2dc824d
Dane Buchanan <daneb@broadbandtap.co.uk>
2023-11-30 20:49:06 +0000

    Updated hamburger styling

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const dropdownMenu = document.getElementById('dropdown-menu');
 
+    // Hide the dropdown menu initially
+    dropdownMenu.style.display = 'none';
+
     hamburgerMenu.addEventListener('click', function () {
         dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
     });
